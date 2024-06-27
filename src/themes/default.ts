759ee0b521d23da45f77e3385dad53091f49ca37
Fellipe97 <lfalmeida97@hotmail.com>
2024-06-26 23:03:33 -0300

    feat: :art: tema para a aplicação

---
+++ src/themes/default.ts
@@ -0,0 +1,27 @@
+export const defaultTheme = {
+    colors: {
+        'white': '#FFFFFF',
+
+        'gray-100': '#F5F5F5',
+        'gray-200': '#CFCFCF',
+
+
+        'blue-600': '#3790ae',
+
+
+
+        
+        'background': '#FFFFFF',
+    },
+    fontWeight: {
+        menu: '900',
+        title: '700',
+        text: '400',
+    },
+    fontSizes: {
+        clock: '24px',
+        title: '22px',
+        button: '16px',
+        text: '12px'
+    },
+}
