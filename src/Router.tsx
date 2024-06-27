82e9aa9a10d117513b27b90ece18c5762d0ab9a2
Fellipe97 <lfalmeida97@hotmail.com>
2024-06-26 23:03:11 -0300

    feat: :sparkles: arquivo de rotas

---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+
+
+export function Router() {
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<Home />}
+            />
+
+            <Route path="*"
+                element={<Home />}
+            />
+        </Routes>
+    )
+}
